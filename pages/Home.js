2093b5050a3aa116afa64e25db99a7f27edba640
Miriam Khan <142635080+maikmixe@users.noreply.github.com>
2024-09-23 14:59:19 -0700

    Add files via upload

---
+++ pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Nonprofit</h1>
+      <p>Learn more about our mission and how you can get involved.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ pages/Programs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Programs() {
+  return (
+    <div>
+      <h1>Our Programs</h1>
+      <p>We offer various programs to support our community.</p>
+    </div>
+  );
+}
+
+export default Programs;
